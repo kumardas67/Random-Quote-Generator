6b20d2caa039e43a19afa960e0cd74ea7aacc93c
Pankaj Das <kumardas67@gmail.com>
2023-10-10 23:19:58 +0530

    Project completed

---
+++ script.js
@@ -0,0 +1,17 @@
+const btnEl = document.getElementById('btn');
+const quoteEl = document.getElementById('quote')
+const authorEl = document.getElementById('author')
+const apiUrl = 'https://api.quotable.io/random';
+
+
+async function getQuote(){
+   const response = await fetch(apiUrl);
+   const data = await response.json()
+   const quoteContent = data.content;
+   const authorQuote = data.author;
+   quoteEl.innerText = quoteContent;
+   authorEl.innerText = authorQuote;
+   console.log(data);
+}
+
+btnEl.addEventListener('click',getQuote)
